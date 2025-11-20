5ef97f188e6b63b8db088eeaa37acd5bec7433f4
Abhijith B <babhijith335@gmail.com>
2025-11-20 15:00:09 +0530

    Update page.jsx

--- Service/client/src/app/success/page.jsx
+++ Service/client/src/app/success/page.jsx
@@ -1,8 +1,8 @@
 "use client";
 
+import axios from "axios";
 import { useSearchParams, useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
-import axios from "axios";
 
 const STATUS = {
   verifying: "Verifying payment...",
