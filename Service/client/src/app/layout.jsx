9c62c5509a7bbeda910c26cd10d0653c747686a9
Abhijith B <babhijith335@gmail.com>
2025-11-20 14:49:47 +0530

    correction

--- Service/client/src/app/layout.jsx
+++ Service/client/src/app/layout.jsx
@@ -1,10 +1,11 @@
 "use client";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import Image from "next/image";
-import Link from "next/link";
 import { Bell, House, Search, ShoppingCart, TicketPercent } from "lucide-react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Geist, Geist_Mono } from "next/font/google";
+import Image from "next/image";
+import Link from "next/link";
+
+import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
